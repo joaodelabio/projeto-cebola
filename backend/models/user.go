4f31eb41089fb1c9f761bf426782d22244783af3
joaodelabio <joaogabriel.delabio@gmail.com>
2024-06-24 17:01:47 -0300

    caio

--- backend/models/user.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type User struct {
-	Username string `json:"username" bson:"username"`
-	Email    string `json:"email" bson:"email"`
-	Password string `json:"password" bson:"password"`
-}
